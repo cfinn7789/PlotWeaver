d38cdaff485557d79e3edcfd32057f1a5c0819cf
cfinn7789 <cfinn7789@gmail.com>
2024-06-06 13:40:35 -0400

    imported server from rsvp project

---
+++ server/server.js
@@ -0,0 +1,45 @@
+// Import the necessary packages
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+// Specify port and initialize express
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Function for setting up and starting the server
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  app.use('/graphql', expressMiddleware(server));
+  
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
